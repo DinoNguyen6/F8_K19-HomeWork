545cf43fc7f8bcc2648b82250ef911b56a9c70a2
DinoNguyen6 <bangnguyendev6@gmail.com>
2026-06-02 23:51:44 +0700

    fix btvn b17

--- BaiTapBuoi17/bai3.js
+++ BaiTapBuoi17/bai3.js
@@ -1,6 +1,6 @@
-const cleanName=(fullName,keyWord)=>{
-    let cleanedName=fullName.toLowerCase().trim();
-    return cleanedName.includes((keyWord.toLowerCase()));
+const cleanName = (fullName, keyWord) => {
+    let cleanedName = fullName.toLowerCase().trim();
+    return cleanedName.includes(keyWord.toLowerCase());
 }
 
 console.log(cleanName('   NGUYEN Van An   ', 'An')); // Mong đợi: true (vì 'nguyen van an' có chứa 'an')
